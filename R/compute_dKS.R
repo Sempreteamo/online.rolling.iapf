84cc83ea6d446a14d2ff87cfda04e6d48d2d0c23
Sempreteamo <84420813+Sempreteamo@users.noreply.github.com>
2024-07-11 10:10:14 +0100

    Update compute_dKS.R

--- R/compute_dKS.R
+++ R/compute_dKS.R
@@ -1,16 +1,20 @@
 compute_dKS <- function(x, w, fks.obj){
-  d <-  dim(x)[2]
+  d <-  ncol(as.matrix(x[1,,]))
   dist <- vector()
   for(t in 1:Time){
     w_ <- normalise_weights_in_log_space(w[t,])[[1]]
-    weighted_mean <- colSums(w_*x[t,,])
-    
+    if(d == 1){
+      weighted_mean <- sum(w_*x[t,,])
+    }else{
+      weighted_mean <- colSums(w_*x[t,,])
+    }
+
     fks_mean <- fks.obj$ahatt[,t]
     fks_cov <- fks.obj$Vt[,,t]
-    
+
     dist[t] <- mahalanobis(weighted_mean, fks_mean, fks_cov)/d
   }
-  
+
   plot(dist)
   return(dist)
 }
