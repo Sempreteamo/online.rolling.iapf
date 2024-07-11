ed3a35b15645484546d49a5040913e651a41b2ad
Sempreteamo <84420813+Sempreteamo@users.noreply.github.com>
2024-07-11 08:03:25 +0100

    Update learn_psi.R

--- R/learn_psi.R
+++ R/learn_psi.R
@@ -16,38 +16,38 @@ learn_psi <- function(x, obs, model){
   d <- output[3]
   psi <- matrix(NA, nrow = Time, ncol = N)
   psi_pa <- matrix(NA, nrow = Time, ncol = 2*d)
-  
+
   #calculate psi
   for(t in Time:1){
 
     if(t == Time){
       for(i in 1:N){
-        dif <- x[t, i,] - obs[t,]
-        
-        psi[t,i] <- (1 / ((2 * pi)^(d / 2))) * 
+        dif <- x[t, i,] - obs[t, drop = FALSE]
+
+        psi[t,i] <- (1 / ((2 * pi)^(d / 2))) *
           exp(-0.5 * t(dif) %*% dif)
       }
-      
-      
+
+
     }else{
-      
+
       for(i in 1:N){
-        
-        psi[t,i] <- exp(evaluate_log_g(model, x[t,i,], obs[t,]))*
+
+        psi[t,i] <- exp(evaluate_log_g(model, x[t,i,], obs[t, drop = FALSE]))*
           exp(evaluate_psi_tilde(x[t,i,], psi_pa[t+1, ], model))
-  
-        
+
+
       }
     }
-    
+
     psi_pa[t,] <- optimize_psi(x[t,,], log(psi[t,]))
-    
-   
+
+
     #print(psi_pa[t, 1:d])
-    #print(obs[t,])
-    
+    #print(obs[t, drop = FALSE])
+
   }
   #print(psi_pa)
   return(params = psi_pa)
-  
+
 }
