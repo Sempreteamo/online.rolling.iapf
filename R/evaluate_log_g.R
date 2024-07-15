f115da4603215ad348b0a9888c881fc21dda1e96
Sempreteamo <84420813+Sempreteamo@users.noreply.github.com>
2024-07-15 20:41:16 +0100

    Update evaluate_log_g.R

--- R/evaluate_log_g.R
+++ R/evaluate_log_g.R
@@ -1,6 +1,6 @@
 #' Function to evaluate the log-density of the observation density
 #'
-#'This function evaluates the log-density of the observation density. 
+#'This function evaluates the log-density of the observation density.
 #'It is expected to be updated to evaluate log-density of general potential functions.
 #'
 #' @param model List containing model parameters
@@ -10,14 +10,14 @@
 #' @return Log-density of the observation density
 #' @export
 #'
-evaluate_log_g <- function(model, x, datum){  
+evaluate_log_g <- function(model, x, datum){
   C <- model$C
   D <- model$D
   d <- length(x)
-  
-  dif <- datum - C%*%x
-  
+
+  dif <- as.vector(datum) - C%*%x
+
   log_density <- (-d/2)*log(2*pi) - (1/2)*log(prod(diag(D))) + (-1/2)*t(dif)%*%D%*%dif
-  return(log_density) 
+  return(log_density)
 }
 
