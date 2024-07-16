9026850adb0cb775785b3074792004a152c67c56
Sempreteamo <84420813+Sempreteamo@users.noreply.github.com>
2024-07-16 06:57:19 +0100

    Update sample_obs.R

--- R/sample_obs.R
+++ R/sample_obs.R
@@ -8,22 +8,22 @@
 #' @return A list containing the observation sequence
 #' @export
 #'
-sample_obs <- function(model, N){
+sample_obs <- function(model){
   A <- model$A
   B <- model$B
   C <- model$C
   D <- model$D
   d <- model$d
 
-  X <- matrix(0, nrow = N, ncol = d)
-  data <- matrix(0, nrow = N, ncol = d)
+  X <- matrix(0, nrow = Time, ncol = d)
+  data <- matrix(0, nrow = Time, ncol = d)
 
   X[1,] <- rnorm(d)
 
   for(t in 2:Time){
     X[t,] <- chol(B)%*%rnorm(d) + A%*%X[t-1,]
   }
-  for(t in 1:N){
+  for(t in 1:Time){
     data[t,] <- rmvn(1, C%*%X[t,], D)
   }
   return(data)
