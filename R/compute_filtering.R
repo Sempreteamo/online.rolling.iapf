239b5fcb07204e3e38ebe62a6631492ebe00a87f
Sempreteamo <84420813+Sempreteamo@users.noreply.github.com>
2024-07-29 21:53:22 +0100

    Update compute_filtering.R

--- R/compute_filtering.R
+++ R/compute_filtering.R
@@ -1,3 +1,15 @@
+#' Function to compute filtering results for Gaussian linear models
+#'
+#' @param model Information of the model
+#' @param data Observations
+#'
+#' @return A list contains:
+#' fkf.obj is the filtering information
+#' fks.obj is the smoothing information
+#'
+#' @export
+#'
+#' @examples
 compute_filtering <- function(model, data){
   a0 <- model$a0
   P0 <- model$P0
@@ -10,6 +22,6 @@ compute_filtering <- function(model, data){
   obs <- data$obs
   fkf.obj <- fkf(a0, P0, dt, ct, Tt, Zt, Ht, Gt, yt = t(obs))
   fks.obj <- fks(fkf.obj)
-  
+
   return(list(fkf.obj, fks.obj))
 }
