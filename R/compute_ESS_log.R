5adbb90f2dd8c4bc8babe76467ca04f0438f5fa8
Sempreteamo <84420813+Sempreteamo@users.noreply.github.com>
2024-07-29 21:32:59 +0100

    Update compute_ESS_log.R

--- R/compute_ESS_log.R
+++ R/compute_ESS_log.R
@@ -1,10 +1,12 @@
-#ess resampling
-
-#logW_un = w[t-1,]
-
+#' Function to generate the ESS
+#'
+#' @param logW_un Unnomalised log weights
+#'
+#' @return Effective sample size
+#' @export
+#'
 compute_ESS_log <- function(logW_un){
-  #print(log_un)
     logW <- normalise_weights_in_log_space(logW_un)[[1]]
     ess <- 1/sum(logW^2)
-    return(ess)  
+    return(ess)
 }
