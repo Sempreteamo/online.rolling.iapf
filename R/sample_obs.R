7404e64ed7945ab4221694c3b42a7d135326cc22
Sempreteamo <84420813+Sempreteamo@users.noreply.github.com>
2024-08-02 12:14:47 +0100

    Update sample_obs.R

--- R/sample_obs.R
+++ R/sample_obs.R
@@ -3,29 +3,34 @@
 #'This function samples synthetic observations for each time step given the state space model.
 #'
 #' @param model List containing model parameters
-#' @param N Number of observations
+#' @param dist Distribution of the observation density
+#' @param Time Total time length
 #'
 #' @return A list containing the observation sequence
 #' @export
 #'
-sample_obs <- function(model){
-  A <- model$A
-  B <- model$B
-  C <- model$C
-  D <- model$D
-  d <- model$d
+sample_obs <- function(model, Time, dist = 'lg'){
+  if(dist = 'lg'){
+    A <- model$A
+    B <- model$B
+    den_mean <- model$den_mean
+    den_cov <- model$den_cov
+    d <- model$d
 
-  X <- matrix(0, nrow = Time, ncol = d)
-  data <- matrix(0, nrow = Time, ncol = d)
+    X <- matrix(0, nrow = Time, ncol = d)
+    data <- matrix(0, nrow = Time, ncol = d)
 
-  X[1,] <- rnorm(d)
+    X[1,] <- rnorm(d)
 
-  for(t in 2:Time){
-    X[t,] <- chol(B)%*%rnorm(d) + A%*%X[t-1,]
+    for(t in 2:Time){
+      X[t,] <- rmvn(1, A%*%X[t-1,], B)
+    }
+
+    for(t in 1:Time){
+      data[t,] <- rmvn(1, den_mean%*%X[t,], den_cov)
+    }
   }
-  for(t in 1:Time){
-    data[t,] <- rmvn(1, C%*%X[t,], D)
-  }
+
   return(data)
 }
 
