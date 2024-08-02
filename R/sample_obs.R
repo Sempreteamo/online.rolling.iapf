7c459dcc32a5ca351a462c6e5caf16c1b330e411
Sempreteamo <84420813+Sempreteamo@users.noreply.github.com>
2024-08-02 22:42:54 +0100

    Update sample_obs.R

--- R/sample_obs.R
+++ R/sample_obs.R
@@ -9,12 +9,8 @@
 #' @return A list containing the observation sequence
 #' @export
 #'
-sample_obs <- function(model, Time, dist = 'lg'){
+sample_obs <- function(tran_mu, tran_cov, Time, den_mean = 0, den_cov = 0, dist = 'lg'){
   if(dist = 'lg'){
-    A <- model$A
-    B <- model$B
-    den_mean <- model$den_mean
-    den_cov <- model$den_cov
     d <- model$d
 
     X <- matrix(0, nrow = Time, ncol = d)
@@ -23,11 +19,13 @@ sample_obs <- function(model, Time, dist = 'lg'){
     X[1,] <- rnorm(d)
 
     for(t in 2:Time){
-      X[t,] <- rmvn(1, A%*%X[t-1,], B)
+      X[t,] <- rmvn(1, tran_mu%*%X[t-1,], tran_cov)
     }
 
     for(t in 1:Time){
       data[t,] <- rmvn(1, den_mean%*%X[t,], den_cov)
+    }else{
+      print('provide the observation data directly')
     }
   }
 
