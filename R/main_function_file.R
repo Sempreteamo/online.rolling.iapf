35b960a2c9219bb4ac86bc6cf6840c78dd8ba0d4
Sempreteamo <84420813+Sempreteamo@users.noreply.github.com>
2024-08-09 13:32:03 +0100

    Update main_function_file.R

--- R/main_function_file.R
+++ R/main_function_file.R
@@ -2,6 +2,7 @@
 #' \dontrun{
 #' The following parameters are provided by users
 library(mvnfast)
+library(FKF)
 Napf = N = 200
 lag = 10
 Time = 10
@@ -17,14 +18,13 @@ ini <- rep(0, d_)
 ini_c  <- alpha^2 / (1-alpha)^2
 tran_c = obs_m = obs_c = diag(1, nrow = d_, ncol = d_)
 parameters_ <- list(k = 5, tau = 0.5, kappa = 0.5)
-obs_params <- list(obs_mean = obs_m, obs_cov = obs_c)
+obs_p <- list(obs_mean = obs_m, obs_cov = obs_c)
 
 output <- generate_blocks(lag, Time)
 breaks_ <- output[[1]]
 psi_index_ <- output[[2]]
 
-model <- list(ini_mu = ini, ini_cov = ini_c, tran_mu = tran_m, tran_cov = tran_c,
-              obs_params = list(obs_mean = obs_m, obs_cov = obs_c),
+model <- list(ini_mu = ini, ini_cov = ini_c, tran_mu = tran_m, tran_cov = tran_c, obs_params = obs_p,
               eval_likelihood = evaluate_likelihood,
 parameters = parameters_, dist = 'lg')
 
