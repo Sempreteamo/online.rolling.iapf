52ee6896119ed11c6109fe7d17bf8f3ecad91db3
Sempreteamo <84420813+Sempreteamo@users.noreply.github.com>
2024-08-09 10:40:23 +0100

    Update compute_filtering.R

--- R/compute_filtering.R
+++ R/compute_filtering.R
@@ -1,7 +1,7 @@
 #' Function to compute filtering results for Gaussian linear model
 #'
 #' @param params Information of the model
-#' @param data Observations
+#' @param obs Observations
 #'
 #' @return A list contains:
 #' fkf.obj is the filtering information
@@ -9,7 +9,7 @@
 #'
 #' @export
 #'
-compute_fkf_filtering <- function(params, obs){
+compute_filtering <- function(params, obs){
   a0 <- params$a0
   P0 <- params$P0
   dt <- params$dt
@@ -18,6 +18,7 @@ compute_fkf_filtering <- function(params, obs){
   Zt <- params$Zt
   Ht <- params$Ht
   Gt <- params$Gt
+  obs <- data$obs
   fkf.obj <- fkf(a0, P0, dt, ct, Tt, Zt, Ht, Gt, yt = t(obs))
   fks.obj <- fks(fkf.obj)
 
