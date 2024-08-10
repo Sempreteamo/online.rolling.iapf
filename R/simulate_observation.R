734cdef149001a652c05586a8d76ff76c72ff2c3
Sempreteamo <84420813+Sempreteamo@users.noreply.github.com>
2024-08-10 23:21:57 +0100

    Create simulate_observation.R

---
+++ R/simulate_observation.R
@@ -0,0 +1,16 @@
+#' Function to simulate observations
+#'
+#' @param state State which to evaluate observations at
+#' @param params Parameters of the observation distribution
+#'
+#' @return
+#' @export
+#'
+#' @examples
+simulate_observation <- function(state, params){
+  den_mean <- params[[1]]
+  den_cov <- params[[2]]
+  obs <- rmvn(1, den_mean%*%state, den_cov)
+  return(obs)
+}
+#' @import mvnfast
