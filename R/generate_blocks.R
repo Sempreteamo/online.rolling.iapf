837b8d423c7a1af54202fc933896126fcc928ca9
Sempreteamo <84420813+Sempreteamo@users.noreply.github.com>
2024-08-27 17:11:01 +0100

    Update generate_blocks.R

--- R/generate_blocks.R
+++ R/generate_blocks.R
@@ -19,11 +19,11 @@ generate_blocks <- function(lag, len){
   breaks[[1]] <- seq(1, len, by = lag)
   breaks[[2]] <- c(1, seq(lag/2 + 1, len, by = lag))
 
-  if (tail( breaks[[1]], 1) != len) {
+  if (utils::tail( breaks[[1]], 1) != len) {
     breaks[[1]] <- c(breaks[[1]], len + 1)
   }
 
-  if (tail(breaks[[2]], 1) != len) {
+  if (utils::tail(breaks[[2]], 1) != len) {
     breaks[[2]] <- c(breaks[[2]], len + 1)
   }
 
