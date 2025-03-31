ab8a070e0061e73ac5781ceca138ac87659777cc
Sempreteamo <84420813+Sempreteamo@users.noreply.github.com>
2025-03-31 20:49:57 +0100

    Update learn_psi.R

--- R/learn_psi.R
+++ R/learn_psi.R
@@ -10,35 +10,29 @@
 #' @return Twisting log_psi function parameters
 #' @export
 #'
-learn_psi <- function(x, obs, model, log_likelihoods){
-  output <- dim(x)
-  Time <- output[1]
-  N <- output[2]
-  d <- output[3]
-  obs <- as.matrix(obs)
-  log_psi <- matrix(NA, nrow = Time, ncol = N)
-  psi_pa <- matrix(NA, nrow = Time, ncol = 2*d)
-
-  #calculate log_psi
-  for(t in Time:1){
-
-    if(t == Time){
-      log_psi[t,] <- log_likelihoods[t,]
-
-    }else{
-
-      for(i in 1:N){
-        log_psi[t,i] <- log_likelihoods[t,i] + evaluate_psi_tilde(x[t,i,], psi_pa[t+1, ], model)
-
-      }
+learn_psi <- function(s, psi_t, H_prev, model){
+  N <- nrow(H_prev$X)
+  d <- ncol(H_prev$X)
+  X_prev <- H_prev$X
+  log_likelihoods <- H_prev$log_li
+  
+  log_psi <- numeric(N)
+  psi_pa <- numeric(2*d)
+  
+  if(all(psi_t == 1)){
+    
+    log_psi <- log_likelihoods
+    
+  }else{
+    for(i in 1:N){
+      log_psi[i] <- log_likelihoods[i] + evaluate_psi_tilde(X_prev[i,], psi_t, model)
+      
     }
-
-    psi_pa[t,] <- optimize_psi(x[t,,], log_psi[t,])
-
-
-
   }
- 
+  
+  psi_pa <- optimize_psi(X_prev, log_psi)
+  
   return(psi_pa = psi_pa)
-
+  
 }
+
