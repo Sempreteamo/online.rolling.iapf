af742a87f5c0a6061a5841a7a87bd72bc0f00f08
Sempreteamo <84420813+Sempreteamo@users.noreply.github.com>
2025-03-31 20:49:26 +0100

    Update orc_smc.R

--- R/orc_smc.R
+++ R/orc_smc.R
@@ -23,15 +23,13 @@ Orc_SMC <- function(lag, data, model, N) {
   w0 <- matrix(log(1/N), 1, N)
   
   logZ_vec <- numeric(Time)
-  logZ_tilde <- numeric(Time)
-  logZ_s  <- 0
   
   X <- X_apf <- array(NA, c(Time, N, d))
   log_W_apf <- log_W <- matrix(NA, Time, N)
   log_likelihoods <- log_likelihoods_apf <- matrix(NA, Time, N)
   filtering_estimates <- matrix(NA, Time, d)
   
-  psi_pa <- matrix(NA, Time, 2*d)
+  psi_pa <- matrix(NA, Time + 1, 2*d)
   
   H <- vector('list', Time + 1)
   H_tilde <- vector('list', Time + 1)
@@ -44,24 +42,26 @@ Orc_SMC <- function(lag, data, model, N) {
     t0 <- max(t - lag + 1, 1)
     
     # Step 3: Init pass with psi ≡ 1
+    psi_pa[t, ] <- rep(1, 2*d)
     
-    output <- run_psi_APF_rolling(data, t, psi_pa, H_tilde[[t]] , model, init = TRUE)
+    output <- run_psi_APF_rolling(data, t, psi_pa[t,, drop = FALSE], H_tilde[[t]] , model, init = TRUE)
     H_tilde[[t+1]] <- output$H
-    log_likelihoods_apf[t,] <- output$log_likelihoods
+    #log_likelihoods_apf[t,] <- output$log_likelihoods
     
     # Step 4: Policy Refinement
+    psi_pa[t+1, ] <- rep(1, 2*d)
+    
     for (k in 1:K) {
       
-      for (s in t0:t) {X_apf[s,,] <- H_tilde[[s + 1]]$X}
-      
-      psi_pa[t0:t,] <- learn_psi(X_apf[t0:t,,, drop = FALSE],
-                                 model, log_likelihoods_apf[t0:t,, drop = FALSE])
+      for (s in t:t0) {
+        psi_pa[s,] <- learn_psi(s, psi_pa[s+1,, drop = FALSE ], H_tilde[[s+1]], model)
+      }
       
       for (s in t0:t) {
-        output <- run_psi_APF_rolling(data, s, psi_pa, H_tilde[[s]], model, init = FALSE)
+        output <- run_psi_APF_rolling(data, s, psi_pa[s,, drop = FALSE], H_tilde[[s]], model, init = FALSE)
         
         H_tilde[[s+1]] <- output$H
-        log_likelihoods_apf[s,] <- output$log_likelihoods
+        #log_likelihoods_apf[s,] <- output$log_likelihoods
       }
     }
     
@@ -69,7 +69,7 @@ Orc_SMC <- function(lag, data, model, N) {
 
     for (s in t0:t) {
       
-      output <- run_psi_APF_rolling(data, s, psi_pa, H[[s]], model, init = FALSE)
+      output <- run_psi_APF_rolling(data, s, psi_pa[s,, drop = FALSE], H[[s]], model, init = FALSE)
       
       H[[s+1]] <- output$H
      
