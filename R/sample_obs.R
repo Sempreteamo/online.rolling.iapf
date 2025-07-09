e82b571fa15facbb5e74002cd62f6e21b92fad8a
Sempreteamo <84420813+Sempreteamo@users.noreply.github.com>
2025-07-10 00:03:10 +0100

    Update sample_obs.R

--- R/sample_obs.R
+++ R/sample_obs.R
@@ -29,7 +29,7 @@ sample_obs <- function(model, Time, d){
 
 
     for(t in 1:Time){
-      data[t,] <- model$simu_observation(X[t,,drop = FALSE], model$obs_params)
+      data[t,] <- model$simu_observation(X[t,], model$obs_params)
 
     }
 
