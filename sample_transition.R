b1ea165e23f2cf0c786612483818e468d56f7dc1
Sempreteamo <84420813+Sempreteamo@users.noreply.github.com>
2025-07-14 19:58:33 +0100

    Delete sample_transition.R

--- sample_transition.R
+++
@@ -1,8 +0,0 @@
-sample_transition <- function(x, model, N){
-  A <- model$tran_mu
-  B <- model$tran_cov
-  ini <- model$ini_mu
-  samples <- mvnfast::rmvn(N, ini + A%*%(x - ini), B)
-
-  return(samples)
-}
